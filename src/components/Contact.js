a8546d9375f3a823aefc4aea4443532f4307bc71
Kyle <79099015+kylewilliamson123@users.noreply.github.com>
2021-09-16 12:20:21 -0500

    hopefully second to last commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import emailjs from 'emailjs-com';
+import {container, col, row} from 'bootstrap'
+
 
 function Contact () {
+
+    function sendEmail(e) {
+        e.preventDefault();
+    
+        emailjs.sendForm('Gmail', 'template_zf6fjnq', e.target, 'user_eLvZJto2Wc9htquw6ehEr')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset();
+      }
+
     return(
         <div className="contact" id='ContactMe'>
-            <p>this is where you can contact me</p>
+            
+            <div className="email-container">
+            <h1>Email Me</h1>
+                <form onSubmit={sendEmail}>
+                    <div className="name">
+                        <input type="text" className="form-control" placeholder="Name" name="name"/>
+                    </div>
+                    <div className="emailAdress">
+                        <input type="email" className="form-control" placeholder="Your Email Adress" name="email"/>
+                    </div>
+                    <div className="eSubject">
+                        <input type="text" className="form-control" placeholder="Subject" name="subject"/>
+                    </div>
+                    <div className="message">
+                        <textarea type="text" className="form-control" cols="30" rows="8" placeholder="Your message" name="message"/>
+                    </div>
+                    <div className="submit">
+                        <input type="submit" id="button" className="btn" value="Send Email"/>
+                    </div>
+                </form>
+            </div>
+            
         </div>
     );
 }
